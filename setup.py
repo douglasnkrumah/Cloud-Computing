5ebaf29028542c6b9b3192ca50568a45dc298090
douglasnkrumah <douglas.nkrumah@live.co.uk>
2017-03-07 12:42:09 +0000

    our new app

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
+
 setup(name='YourAppName',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      install_requires=['Flask','flask-wft','flask-babel','markdown','flup','Flask>=0.7.2', 'MarkupSafe', 'unirest'],
       )
