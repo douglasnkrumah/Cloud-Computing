fcf79be875296e13e4d72ebdb35f05651f1b667d
douglasnkrumah <douglas.nkrumah@live.co.uk>
2017-03-28 12:52:33 +0100

    our new app

--- wsgi/app/__init__.py
+++ wsgi/app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-app.secret_key = 'ZoltaR2'
+app.secret_key = os.environ['MY_SECRET_KEY']
 
-from app import views
-
+from app import views, simple, database
