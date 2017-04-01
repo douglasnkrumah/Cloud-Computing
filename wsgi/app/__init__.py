38fa854f1926ead4ad99cb9cd8b91ba605af5090
douglasnkrumah <douglas.nkrumah@live.co.uk>
2017-04-01 15:52:21 +0100

    test

--- wsgi/app/__init__.py
+++ wsgi/app/__init__.py
@@ -3,6 +3,12 @@ import os
 
 app = Flask(__name__)
 
-app.secret_key = os.environ['MY_SECRET_KEY']
 
-from app import views, simple, database
+#app.secret_key = os.environ['MY_SECRET_KEY']
+app.secret_key = "yoyoy"
+
+from app import views 
+
+from app import simple
+ 
+from app import database
--- wsgi/app/database.py
+++ wsgi/app/database.py
@@ -3,17 +3,17 @@ from flask_pymongo import PyMongo
 from flask import jsonify
 import os
 
-	app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
-	app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']
+app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
+app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']
 
-	mongo = PyMongo(app)
+mongo = PyMongo(app)
 
-	@app.route('/database/collections', methods=['GET'])
-	def get_all_databases():
+@app.route('/database/collections', methods=['GET'])
+def get_all_databases():
 	return jsonify({'result' : mongo.db.collection_names()})
 
-	@app.route('/database/personnel', methods=['GET'])
-	def get_all_personnel():
+@app.route('/database/personnel', methods=['GET'])
+def get_all_personnel():
 	collection = mongo.db.test
 	output = []
 	for doc in collection.find():
